b004e1cc5d2291f02e5675826c44864800d0eb1e
saurabh <support@technofra.com>
2025-02-19 13:01:24 +0530

    added functionality for adding and updating invoice details from admin side

---
+++ app/Http/Controllers/EnvoiceController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrdersTrack;
+use App\Models\OrderTrack;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class EnvoiceController extends Controller
+{
+    public function addInvoice(Request $request)
+    {
+        // Validate the request
+        $validations = Validator::make($request->all(), [
+            'enquiry_id' => 'required',
+            'courier_name' => 'required',
+            'courier_number' => 'required|numeric',
+            'courier_website' => 'required',
+            'invoice_file' => 'required|mimes:pdf|max:10240',
+        ]);
+
+        // Check validation errors
+        if ($validations->fails()) {
+            return back()->withErrors($validations)->withInput();
+        }
+
+        // Create new product
+        $invoice = new OrdersTrack();
+        $invoice->enquiry_id = $request->enquiry_id;
+        $invoice->courier_name = $request->courier_name;
+        $invoice->courier_number = $request->courier_number;
+        $invoice->courier_website = $request->courier_website;
+
+        // Handle PDF upload
+        if (!empty($request->invoice_file)) {
+            $image2 = $request->invoice_file;
+            $ext2 = $image2->getClientOriginalExtension();
+            $imageName2 = time() . "." . $ext2;
+            $image2->move(public_path('uploads/invoices/'), $imageName2);
+            $invoice->invoice_file = $imageName2;
+        }
+        $invoice->save();
+
+        flash()->success('Invoice Details Added Successfully.');
+        return redirect()->route('admin.order');
+    }
+
+    public function updateInvoice(Request $request)
+    {
+        // Validate the request
+        $validations = Validator::make($request->all(), [
+            'invoice_id' => 'required',
+            'enquiry_id' => 'required',
+            'courier_name' => 'required',
+            'courier_number' => 'required|numeric',
+            'courier_website' => 'required',
+            'invoice_file' => 'mimes:pdf|max:10240',
+        ]);
+
+        // Check validation errors
+        if ($validations->fails()) {
+            return back()->withErrors($validations)->withInput();
+        }
+
+        // Create new product
+        $invoice = OrdersTrack::find($request->invoice_id);
+        $invoice->enquiry_id = $request->enquiry_id;
+        $invoice->courier_name = $request->courier_name;
+        $invoice->courier_number = $request->courier_number;
+        $invoice->courier_website = $request->courier_website;
+
+        // Handle PDF upload
+        if (!empty($request->invoice_file)) {
+            $image2 = $request->invoice_file;
+            $ext2 = $image2->getClientOriginalExtension();
+            $imageName2 = time() . "." . $ext2;
+            $image2->move(public_path('uploads/invoices/'), $imageName2);
+            $invoice->invoice_file = $imageName2;
+        }
+        $invoice->save();
+
+        flash()->success('Invoice Details Updated Successfully.');
+        return redirect()->route('admin.order');
+    }
+}
